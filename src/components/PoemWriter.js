5fdee492aed912433adf10bf3d6d90c9185dc4bf
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-09-20 09:48:15 -0400

    Done

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,64 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  isValidPoem = () => {
+    const poem = this.state.content
+    // The poem has three lines.
+    // The first line has five words.
+    // The second line has three words.
+    // The third line has five words.
+    const lines = poem.split("\n")
+    let isValid = false
+    let linesValid = false
+
+    if (lines.length === 3){
+      for (let i = 0; i < lines.length; i++){
+        let mystring = lines[i]
+        mystring = mystring.replace(/^\s+|\s+$/g, "").split(" ")
+        if (i === 0 && mystring.length === 5){
+          continue
+        }
+        if (i === 1 && mystring.length === 3) {
+          continue
+        }
+        if (i === 2 && mystring.length === 5) {
+          isValid = true
+          linesValid = true
+        } else {
+          break
+        }
+      }
+    }
+
+    if (isValid === true && linesValid === true) {
+      return true
+    } else {
+      return false
+    }
+
   }
 
   render() {
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
+        <textarea
+          rows="3"
+          cols="60"
+          onChange={this.handleChange}
+          onClick={this.isValidPoem}
         />
-        <div 
-          id="poem-validation-error" 
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
+      {(this.isValidPoem()) ? "" : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure! </div>}
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      currentChars: "",
+      maxChars: 140
+    };
   }
+
+  handleChange = (event) => {
+    this.setState({
+      currentChars: event.target.value
+    })
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input onChange={this.handleChange} type="text" value={this.state.currentChars}/>
+        <p>Remaining characters: {this.state.maxChars - this.state.currentChars.length}</p>
       </div>
     );
   }
